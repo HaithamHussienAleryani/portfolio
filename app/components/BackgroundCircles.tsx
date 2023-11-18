98f5a88a99f765c2da822913a4db5c8ae719b9d1
Haitham Hussien Aleryani <81069558+HaithamHussienAleryani@users.noreply.github.com>
2023-11-18 19:42:52 +0300

    fix hero small design

--- app/components/BackgroundCircles.tsx
+++ app/components/BackgroundCircles.tsx
@@ -16,14 +16,14 @@ const BackgroundCircles = () => {
 
             className={'relative flex justify-center items-center z-0 mt-52'}>
             <div
-                className={' absolute  border-primary opacity-10 border mx-auto mt-52 w-[200px] h-[200px] rounded-full animate-ping'}/>
+                className={' absolute  border-primary opacity-10 border mx-auto mt-52 w-[100px] h-[100px] md:w-[200px] md:h-[200px] rounded-full animate-ping'}/>
             <div
-                className={' absolute border-primary opacity-25 border mx-auto mt-52 w-[300px] h-[300px] rounded-full animate-ping'}/>
+                className={' absolute border-primary opacity-25 border mx-auto mt-52 w-[200px] h-[200px] md:w-[300px] md:h-[300px] rounded-full animate-ping'}/>
             <div
-                className={' absolute border-primary opacity-60 border mx-auto mt-52 w-[400px] h-[400px] rounded-full animate-ping'}/>
+                className={' absolute border-primary opacity-60 border mx-auto mt-52 w-[300px] h-[300px] md:w-[400px] md:h-[400px] rounded-full animate-ping'}/>
 
             <div
-                className={' absolute border-primary border mx-auto opacity-20 mt-52 w-[800px] h-[800px] rounded-full animate-pulse'}/>
+                className={' absolute border-primary border mx-auto opacity-20 mt-52 w-[500px] h-[500px] md:w-[800px] md:h-[800px] rounded-full animate-pulse'}/>
 
         </motion.div>
     );
